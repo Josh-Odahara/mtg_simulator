7b37118422280e2d486fcf98365b10cb060626f2
Josh Odahara <josh.odahara@gmail.com>
2026-07-07 01:58:39 -0700

    add: hardcoded life total to player struct

--- lib/player.ex
+++ lib/player.ex
@@ -1,11 +1,11 @@
 defmodule MTGSimulator.Player do
   alias MTGSimulator.Game
 
-  defstruct [:name, :hand, :deck, :life]
+  defstruct [:name, :hand, :deck, life: 100]
 
-  def new_player(name, life) do
+  def new_player(name) do
     {hand, deck} = Game.new_deck() |> Game.shuffle_deck() |> Game.deal_hand()
 
-    %MTGSimulator.Player{name: name, hand: hand, deck: deck, life: life}
+    %MTGSimulator.Player{name: name, hand: hand, deck: deck, life: 100}
   end
 end
